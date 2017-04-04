6f8187902722baf080150b0e2873181465868f74
lionescu <lrionescu26@gmail.com>
2017-04-04 15:30:32 +0100

    Extract finding best matches

--- application/main_runner.py
+++ application/main_runner.py
@@ -5,6 +5,30 @@ from sklearn.metrics.pairwise import linear_kernel
 
 from application.article_processor import process_articles
 
+
+def print_best_three(article_path, similarity):
+    i = 0
+    for root, dr, filepath in os.walk(article_path):
+        for file in filepath:
+            best_matches = [];
+            similarity_list = similarity[i].tolist()
+            best_three = sorted(similarity_list)[-4:-1]
+
+            j = 0
+            while j < len(similarity_list):
+                if similarity_list[j] in best_three and similarity_list[j] > 0:
+                    best_matches.append(filepath[j])
+                j += 1
+
+            print('max similarity: ')
+            print(best_three)
+
+            print(file + ' most similar with: ')
+            print(best_matches)
+            print('.......................................')
+            i += 1
+
+
 if __name__ == '__main__':
 
     article_path = sys.argv[1]
@@ -19,25 +43,4 @@ if __name__ == '__main__':
 
     similarity = linear_kernel(x_train, x_train)
 
-    i = 0
-    for root, dir, filepath in os.walk(article_path):
-        for file in filepath:
-            max_similarity = 0;
-            best_matches = [];
-            j = 0
-
-            similarity_list = similarity[i].tolist()
-            best_three = sorted(similarity_list)[-4:-1]
-
-            while j < len(similarity_list):
-                if similarity_list[j] in best_three and similarity_list[j] > 0:
-                    best_matches.append(filepath[j])
-                j += 1
-
-            print('max similarity ')
-            print(best_three)
-
-            print(file + ' most similar with ')
-            print(best_matches)
-            i += 1
-            print('.......................................')
+    print_best_three(article_path, similarity)
