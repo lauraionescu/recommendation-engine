9599c46b76025643b3cd8459a1661c2645ee48ed
lionescu <lrionescu26@gmail.com>
2017-04-04 15:26:41 +0100

    Output three best matches

--- application/main_runner.py
+++ application/main_runner.py
@@ -1,12 +1,7 @@
-import json, os, re, sys
-
-from nltk.collocations import *
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
+import os, sys
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
+from sklearn.metrics.pairwise import linear_kernel
 
 from application.article_processor import process_articles
 
@@ -22,24 +17,27 @@ if __name__ == '__main__':
 
     x_train = vectorizer.fit_transform(dataset)
 
-    cosine_similarity = linear_kernel(x_train, x_train)
+    similarity = linear_kernel(x_train, x_train)
 
     i = 0
     for root, dir, filepath in os.walk(article_path):
         for file in filepath:
             max_similarity = 0;
-            best_match = 'unknown';
+            best_matches = [];
             j = 0
 
-            cosine_similarity_list = cosine_similarity[i].tolist()
+            similarity_list = similarity[i].tolist()
+            best_three = sorted(similarity_list)[-4:-1]
 
-            while j < len(cosine_similarity_list):
-                if cosine_similarity_list[j] > max_similarity and cosine_similarity_list[j] < 0.999:
-                    max_similarity = cosine_similarity_list[j]
-                    best_match = filepath[j]
+            while j < len(similarity_list):
+                if similarity_list[j] in best_three and similarity_list[j] > 0:
+                    best_matches.append(filepath[j])
                 j += 1
 
-            print('max similarity ', max_similarity)
+            print('max similarity ')
+            print(best_three)
 
-            print(file + ' most similar with ' + best_match)
+            print(file + ' most similar with ')
+            print(best_matches)
             i += 1
+            print('.......................................')
